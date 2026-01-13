add176ed48ab7056f5e2fb560ec64dec93cbbfa0
KaziNazmusSakib <kazinazmussakib36@gmail.com>
2026-01-13 18:31:58 +0600

    backend-01: update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
-import { AuthModule } from './auth/auth.module';
-import { AdminModule } from './admin/admin.module';
-import { BuyerModule } from './buyer/buyer.module';
-import { SellerModule } from './seller/seller.module';
-import { SupplierModule } from './supplier/supplier.module';
-import { ProductModule } from './product/product.module';
-import { OrderModule } from './order/order.module';
-import { CategoryModule } from './category/category.module';
-import { CartModule } from './cart/cart.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { ReviewModule } from './review/review.module';
-import { TransactionsModule } from './transactions/transactions.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync(typeOrmConfig),
-    AuthModule,
-    AdminModule,
-    BuyerModule,
-    SellerModule,
-    SupplierModule,
-    ProductModule,
-    OrderModule,
-    CategoryModule,
-    CartModule,
-    NotificationsModule,
-    ReviewModule,
-    TransactionsModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
