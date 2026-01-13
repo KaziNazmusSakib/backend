47a17103cf7703d141048aff93009fd593e76083
KaziNazmusSakib <kazinazmussakib36@gmail.com>
2026-01-13 18:51:45 +0600

    backend-02:app

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
+  // port: parseInt(process.env.DATABASE_PORT, 10),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
